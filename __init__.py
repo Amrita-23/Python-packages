8e86017ecaf8259a25b862ed32de5f8d0d213a19
amrita-23 <amritaptrehan@gmail.com>
2025-11-21 10:50:29 +0000

    __init__ for Python Packages

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from . import Sequences as Sequences
+from . import Fasta as Fasta
+#Now, you can import in your script by using from Package import Module and use classes by referring to Module.className.
